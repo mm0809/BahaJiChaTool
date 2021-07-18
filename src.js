63c0236b330f26f464aaee0c329173a53f07a749
mm0809 <37280643+mm0809@users.noreply.github.com>
2021-07-19 02:22:40 +0800

    Update src.js

--- src.js
+++ src.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Baha JiCha tool
-// @namespace    http://tampermonkey.net/
+// @namespace    GiCha
 // @version      0.1
 // @description  try to take over the world!
 // @author       opmm0809
